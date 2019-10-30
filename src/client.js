cb48f8ddd83fd468cd26dce2bb29e19f289da969
ostrebler <strebler.olivier@hotmail.fr>
2019-10-30 01:38:21 +0100

    dev

--- src/client.js
+++ src/client.js
@@ -3,6 +3,7 @@ import Observable from 'zen-observable'
 import lodash from 'lodash'
 import { applyPatch } from 'rfc6902'
 
+// The main client class
 function Client(config) {
   this.config = {
     url: `${window.location.origin}`,
@@ -13,64 +14,67 @@ function Client(config) {
     ...config
   }
 
-  this.subscriptions = {}
-
   // The underlying socket.io client
   this.socket = makeSocket(this.config.url, {
     path: this.config.path,
     ...this.config.io
   })
-
-  this.socket.on('patch', this.handlePatch.bind(this))
 }
 
+// When one fires a query...
 Client.prototype.query = function(query, input, context) {
+  // ...the complete context is calculated...
+  context = {
+    $live: true,
+    ...context,
+    ...this.config.context(context, { query, input })
+  }
+
+  // ...if no live update is required, then a simple promise is returned
+  // resolving to the query's output
+  if(!context.$live)
+    return new Promise(resolve => {
+      this.socket.emit('query', query, input, context, output => {
+        resolve(output)
+      })
+    })
+  // ...otherwise an observable is created...
   return new Observable(observer => {
     let subscriptionId = null
-    const finalContext = {
-      $live: true,
-      ...context,
-      ...this.config.context(context, { query, input })
-    }
-    this.socket.emit('query', query, input, finalContext, (output, id) => {
+    this.socket.emit('query', query, input, context, (output, id) => {
       if(!observer.closed) {
+        subscriptionId = id
         observer.next(output)
-        if(!finalContext.$live)
-          observer.complete()
-        else {
-          subscriptionId = id
-          this.subscriptions[id] = delta => {
-            applyPatch(output, delta)
-            output = lodash.clone(output)
-            observer.next(output)
-          }
-        }
+        // ...subscribing to patches and generating observable values on each update
+        this.socket.on(`patch/${id}`, delta => {
+          applyPatch(output, delta)
+          output = lodash.clone(output)
+          observer.next(output)
+        })
       }
     })
     return () => {
-      if(finalContext.$live && subscriptionId) {
-        delete this.subscriptions[subscriptionId]
+      if(subscriptionId) {
+        this.socket.off(`patch/${subscriptionId}`)
         this.socket.emit('unquery', subscriptionId)
       }
     }
   })
 }
 
+// When one fires an action...
 Client.prototype.action = function(action, input, context) {
+  // ...the complete context is calculated...
   context = {
     ...context,
     ...this.config.context(context, { action, input })
   }
+  // ...and a promise is returned, resolving to the action's output
   return new Promise(resolve => {
     this.socket.emit('action', action, input, context, output => {
       resolve(output)
     })
   })
-}
-
-Client.prototype.handlePatch = function(id, delta) {
-  if(this.subscriptions[id])
-    this.subscriptions[id](delta)
 }
 
 export default Client
